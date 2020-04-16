126d2ccd94a9f12a0159e4a9ce0f093c889c1aef
Kaitlyn <ricek20@wwu.edu>
2020-04-16 15:11:46 -0700

    comment added

--- src/main/java/lab1/Hello.java
+++ src/main/java/lab1/Hello.java
@@ -2,6 +2,7 @@ package lab1;
 
 public class Hello {
   public static void main(String[] args) {
+//Print out Hello with 0th command line argument
     System.out.println("Hello, " + args[0] + "!");
   }
 }
